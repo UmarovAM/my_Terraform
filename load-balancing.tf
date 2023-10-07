f897c9bae8eb3ce8d9632c7c3007eb9a02e2bbcc
UmarovAM <118117183+UmarovAM@users.noreply.github.com>
2023-10-07 23:32:04 +0500

    Update load-balancing.tf

--- load-balancing.tf
+++ load-balancing.tf
@@ -90,9 +90,9 @@ output "internal-vm-1" {
 output "external-vm-1" {
   value = "${yandex_compute_instance.vm[0].network_interface.0.nat_ip_address}"
 }
-output "internal-vm-1" {
+output "internal-vm-2" {
   value = "${yandex_compute_instance.vm[1].network_interface.0.ip_address}"
 }
-output "external-vm-1" {
+output "external-vm-2" {
   value = "${yandex_compute_instance.vm[1].network_interface.0.nat_ip_address}"
 }
